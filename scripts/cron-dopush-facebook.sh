084616dabc466c2a7cca474761444772800d421c
eric <eric@hellman.net>
2019-09-30 20:48:06 -0400

    don't leave open shells

--- scripts/cron-dopush-facebook.sh
+++ scripts/cron-dopush-facebook.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 #
 cd /export/sunsite/users/gutenbackend/ebookconverter
-~/.local/bin/pipenv shell
-ebookconverter -v -v --range=1- --goback=24 --build=facebook
+~/.local/bin/pipenv run ebookconverter -v -v --range=1- --goback=24 --build=facebook
