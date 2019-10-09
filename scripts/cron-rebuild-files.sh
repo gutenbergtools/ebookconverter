04a26dc96af722b48c751a8c03033fc0f1a9ab85
eric <eric@hellman.net>
2019-10-09 17:52:28 -0400

    update rebuild scripts

--- scripts/cron-rebuild-files.sh
+++ scripts/cron-rebuild-files.sh
@@ -3,6 +3,7 @@
 
 D=$(date +"%-d")
 
+# will rebuild up to PG 65100 in a 31 day month
 BOOKS_PER_DAY=2100
 
 START=$((($D-1)*$BOOKS_PER_DAY))
@@ -10,4 +11,5 @@ STOP=$(($D*$BOOKS_PER_DAY-1))
 
 echo "Invoking ebookconverter for range $START to $STOP ..."
 
-ebookconverter -v -v --range=$START-$STOP --build=all --jobs=50 --pidfile=/tmp/pg-cron-rebuild.pid
+cd /export/sunsite/users/gutenbackend/ebookconverter
+~/.local/bin/pipenv run ebookconverter -v -v --range=$START-$STOP --build=all --jobs=50 --pidfile=/tmp/pg-cron-rebuild.pid
