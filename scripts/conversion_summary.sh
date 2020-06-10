8b24ed5b128fe3d767776faa1ea9cc533df1a665
eric <eric@hellman.net>
2020-06-10 14:19:20 -0400

    add kindlegen and too-deep reports to conversion_summary

--- scripts/conversion_summary.sh
+++ scripts/conversion_summary.sh
@@ -3,3 +3,5 @@
 
 grep 'Missing file' ebookconverter.log > missingfiles.txt
 grep 'Failed' ebookconverter.log > conversionfails.txt
+grep -C1 "kindlegen: E" ebookconverter.log > kindlegen.txt
+grep 'Omitted file' ebookconverter.log | sort --unique > too_deep.txt
