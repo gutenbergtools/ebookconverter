25e38a03e0871ef05b0981e16d59d6fa2dbab40b
eric <eric@hellman.net>
2020-09-15 17:27:02 -0400

    update scripts

--- scripts/cron-dopush.sh
+++ scripts/cron-dopush.sh
@@ -1,30 +1,40 @@
-#!/bin/bash
-#
 
-# echo "cron-dopush: checking for files ..."
+export PUBLIC="${VHOST}/html"
+export PHP="php -c ${PRIVATE}/lib/php/"
 
-cd /export/sunsite/users/gutenbackend/ebookconverter
-
-# Load environment variables:
-. ./.env
 
 # First check whether there are any .trig files in 
 #  /public/vhost/g/gutenberg/private/logs/dopush/
+LIST=`/bin/ls -1 /public/vhost/g/gutenberg/private/logs/dopush/ | grep .trig | sed 's/\.zip.trig//'`
+
+# No .trig files? Exit:
+if [ "${LIST}x" = x ] ; then
+#   echo DEBUG: empty list is $LIST .. exiting
+    exit 0
+fi
+
+# echo DEBUG: non-empty list is $LIST 
+# exit 0
+
+# This does the basic identification of files and metadata,
+# extracted from the .zip files identified by the .trig files:
 ~/.local/bin/pipenv --bare run fileinfo | ${PHP} ${PRIVATE}/lib/python/autocat/autocat.php || exit 1
 
 # We have work to do! 
-echo "do_push: making files ..."
+# echo "do_push: making files ..."
 
 ~/.local/bin/pipenv run autodelete
 
-echo "ran autodelete"
+# echo "ran autodelete"
 
-~/.local/bin/pipenv run ebookconverter -v --range=1- --goback=24 --make=all
+# gbn 2020-04-03: "goback-24" runs the last 24 hours. Instead, we
+# will expicitly rebuild every item in the LIST:
+# ~/.local/bin/pipenv run ebookconverter -v --range=1- --goback=24 --make=all
+for i in ${LIST}; do
+    ~/.local/bin/pipenv run ebookconverter -v --range=${i} --build=all
+done
 
-echo "ran ebookconverter"
 
-exit
-sleep 300
 
-wget -O ${PUBLIC}/cache/latest-covers.html.utf8 "http://gutenberg2:8000/covers/small/latest/10"
-gzip -c ${PUBLIC}/cache/latest-covers.html.utf8 > $PUBLIC/cache/latest-covers.html.utf8.gzip
+#hely 2020-04-29: This pushes the latest book covers to the jekyll site generator
+./cron-latesttitles.sh
