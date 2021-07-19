6c2ba0d2d653ea296ca13e6e9370f6290c9b4528
eric <eric@hellman.net>
2021-07-19 14:35:50 -0400

    add candidates test

---
+++ ebookconverter/tests/test_candidates.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import os
+import unittest
+
+
+from .. import Candidates
+
+class TestCandidates(unittest.TestCase):
+    def setUp(self):
+        self.ebook = 4554
+
+    def test_read_from_database(self):
+        candidates = Candidates.Candidates()
+        result = candidates.read_from_database(self.ebook)
+        self.assertTrue(len(result) > 2)
+
+    def test_filter_sort(self):
+        typeglob_list = ('html/utf-8', 'html/iso-8859-*', 'html/*', '*/*')
+        candidates = Candidates.Candidates()
+        files = candidates.read_from_database(self.ebook)
+        filtered = Candidates.Candidates.filter_sort(typeglob_list, files, lambda x: x.format)
+        print(filtered[0])
+
