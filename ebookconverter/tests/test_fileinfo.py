e1f6b948895fd08878121eccec2a7247460a5de3
eric <eric@hellman.net>
2021-08-04 17:06:04 -0400

    tests

--- ebookconverter/tests/test_fileinfo.py
+++ ebookconverter/tests/test_fileinfo.py
@@ -3,6 +3,7 @@
 import os
 import unittest
 
+from libgutenberg.DBUtils import author_exists, ebook_exists, remove_ebook
 
 from .. import FileInfo
 
@@ -11,12 +12,24 @@ class TestFileInfo(unittest.TestCase):
         self.test_file = os.path.join(os.path.dirname(__file__),'4554-h.htm')
         self.test_file2 = os.path.join(os.path.dirname(__file__),'4554.txt')
         self.test_file3 = os.path.join(os.path.dirname(__file__),'4554-0.txt')
+        self.test_fakebook = os.path.join(os.path.dirname(__file__),'99999-h.htm')
 
     def test_scan_file(self):
-         result = FileInfo.scan_file(self.test_file)
+         result = FileInfo.scan_file(self.test_file, 4554)
          self.assertTrue(result)
-         result = FileInfo.scan_file(self.test_file2)
+         result = FileInfo.scan_file(self.test_file2, 4554)
          self.assertTrue(result)
-         result = FileInfo.scan_file(self.test_file3)
+         result = FileInfo.scan_file(self.test_file3, 4554)
          self.assertTrue(result)
+         result = FileInfo.scan_file(self.test_file3, 99999)
+         self.assertFalse(result)
+
+    def test_save_metadata(self):
+        result = FileInfo.scan_file(self.test_fakebook, 99999)
+        self.assertTrue(result)
+        self.assertTrue(ebook_exists(99999))
+        self.assertTrue(author_exists("Lorem Ipsum Jr."))
+        remove_ebook(99999)
+        self.assertFalse(ebook_exists(99999))
+        self.assertTrue(author_exists("Hemingway, Ernest"))
 
