2959a3390dfaccf186a3094b764f31b283371c22
eric <eric@hellman.net>
2021-10-21 11:51:13 -0400

    delint

--- ebookconverter/Candidates.py
+++ ebookconverter/Candidates.py
@@ -23,12 +23,12 @@ from libgutenberg.Models import File
 
 ob = GutenbergDatabase.Objectbase(False)
 
-class Candidates (object):
+class Candidates(object):
     """ Class to get build candidates from the PG database.
 
     """
 
-    def read_from_database (self, ebook):
+    def read_from_database(self, ebook):
         """ Read candidates from PG database. """
 
         candidates = []
@@ -42,33 +42,33 @@ class Candidates (object):
 
         for file_ in files:
             if (ebook > 10000 and file_.file_type == 'html'
-                and not os.path.basename (file_.archive_path).startswith (str (ebook))):
+                and not os.path.basename(file_.archive_path).startswith(str(ebook))):
                 # must have the form 12345-h.htm (not eg. glossary.htm)
                 continue
             if file_.file_type is None:
                 continue
 
-            adir = gg.archive_dir (ebook)
-            if file_.archive_path.startswith (adir):
-                file_.archive_path = file_.archive_path.replace (adir, 'files/%d' % ebook)
-            elif file_.archive_path.startswith ('etext'):
+            adir = gg.archive_dir(ebook)
+            if file_.archive_path.startswith(adir):
+                file_.archive_path = file_.archive_path.replace(adir, 'files/%d' % ebook)
+            elif file_.archive_path.startswith('etext'):
                 file_.archive_path = 'dirs/' + file_.archive_path
 
             file_.format = "%s/%s" % (file_.fk_filetypes, file_.encoding or 'unknown')
-            candidates.append (file_)
+            candidates.append(file_)
 
         return candidates
 
 
     @staticmethod
-    def filter_sort (typeglob_list, candidates, f):
+    def filter_sort(typeglob_list, candidates, f):
         """ Filter and sort a list of candidates into preference order.
 
         typeglob_list is a list in the form:
           ('html/utf-8', 'html/iso-8859-*', 'html/*', '*/*')
         where the first entry is the preferred format.
 
-        f (candidate) extracts type from candidate
+        f(candidate) extracts type from candidate
 
         example: f = lambda x: x.format if not x.generated else 'skip_this'
 
@@ -78,8 +78,8 @@ class Candidates (object):
 
         for typeglob in typeglob_list:
             for candidate in candidates:
-                if fnmatch.fnmatch (f (candidate), typeglob):
+                if fnmatch.fnmatch(f(candidate), typeglob):
                     if candidate not in result:
-                        result.append (candidate)
+                        result.append(candidate)
 
         return result
